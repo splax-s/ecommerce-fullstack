8e230a50cd051a31ec343c56cebc73d55504ebed
Ikhanoba Michael-Shaka <shakaikhanoba@gmail.com>
2023-01-02 22:50:34 +0100

    feat: finished ui client app for web users

--- admin/src/utils/axiosconfig.js
+++ admin/src/utils/axiosconfig.js
@@ -4,7 +4,7 @@ const getTokenFromLocalStorage = localStorage.getItem("user")
 
 export const config = {
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYWRiMzhhMmY3YjIxZGNmZDUzYTQ3ZSIsImlhdCI6MTY3MjM2MzM4MywiZXhwIjoxNjcyNDQ5NzgzfQ.TZ5ywYEt4it5zNo8331cNwxfvwfyN7jV01kFpNBlYSI`,
+    Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
     Accept: "application/json",
   },
 };
